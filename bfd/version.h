0078ba87f134130dd06ebcf2f4f6bc49d95113f5
Victor Chong <victor.chong@linaro.org>
2013-12-24 14:14:35 -0600

    version.h: Bump version.

--- bfd/version.h
+++ bfd/version.h
@@ -1,4 +1,4 @@
 #define BFD_VERSION_DATE 20131224
 #define BFD_VERSION @bfd_version@
-#define BFD_VERSION_STRING  @bfd_version_package@ @bfd_version_string@ " Linaro 2013.12"
+#define BFD_VERSION_STRING  @bfd_version_package@ @bfd_version_string@ " Linaro 2013.12-1-git"
 #define REPORT_BUGS_TO @report_bugs_to@
