9a014a685bd67390e92bf09cb5065c6dcd0c0642
Will Newton <will.newton@linaro.org>
2014-07-03 17:23:15 +0100

    bfd/version.h: Update version for Linaro 2.24 2014.07 release

--- bfd/version.h
+++ bfd/version.h
@@ -1,4 +1,4 @@
 #define BFD_VERSION_DATE 20140703
 #define BFD_VERSION @bfd_version@
-#define BFD_VERSION_STRING  @bfd_version_package@ @bfd_version_string@ " Linaro 2014.06-1-git"
+#define BFD_VERSION_STRING  @bfd_version_package@ @bfd_version_string@ " Linaro 2014.07"
 #define REPORT_BUGS_TO @report_bugs_to@
